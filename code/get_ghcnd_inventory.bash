d09e298f618c6d43ae18bddd96531abe064b1e6c
Ibrahims_Lab <myola4real@gmail.com>
2025-04-05 11:39:14 -0700

    Initialized snakemake workflow

---
+++ code/get_ghcnd_inventory.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -o data/gghcnd-inventory.txt https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.txt
+
---
+++ code/get_ghcnd_stations.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -o data/ghcnd-stations.txt https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt
